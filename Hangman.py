1b37950dff884b853ed951871f684a347ce0f212
Abdullah Safi <safiabdullah769@gmail.com>
2025-11-01 15:33:41 +0430

    added a function and ask fucntion as well.

--- Hangman.py
+++ Hangman.py
@@ -1,161 +1,172 @@
 import random 
 
+def hangMan_game():
 
 
-print('''
-  _                                             
- | |                                            
- | |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
- | '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
- | | | | (_| | | | | (_| | | | | | | (_| | | | |
- |_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
-                     __/ |                      
-                    |___/                       
-''')
+    print('''
+      _                                             
+    | |                                            
+    | |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+    | '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+    | | | | (_| | | | | (_| | | | | | | (_| | | | |
+    |_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                        __/ |                      
+                        |___/                       
+    ''')
 
-print("🎮 Welcome to the Hangman Game 🎯")
-print("Guess the word before you run out of lives!\n")
+    print("🎮 Welcome to the Hangman Game 🎯")
+    print("Guess the word before you run out of lives!\n")
 
-stages = [
-    """
-      +---+
-      |   |
-      O   |
-     /|\\  |
-     / \\  |
-          |
-    =========
-    """,
-    """
-      +---+
-      |   |
-      O   |
-     /|\\  |
-     /    |
-          |
-    =========
-    """,
-    """
-      +---+
-      |   |
-      O   |
-     /|\\  |
-          |
-          |
-    =========
-    """,
-    """
-      +---+
-      |   |
-      O   |
-     /|   |
-          |
-          |
-    =========
-    """,
-    """
-      +---+
-      |   |
-      O   |
-      |   |
-          |
-          |
-    =========
-    """,
-    """
-      +---+
-      |   |
-      O   |
-          |
-          |
-          |
-    =========
-    """,
-    """
-      +---+
-      |   |
-          |
-          |
-          |
-          |
-    =========
-    """
-]
-
-
-
-lives = 6
-
-word_list = ["lamborghini", "porsche", "ferrari", "bugatti", "mcLaren", "astonMartin", "rollsRoyce", "bentley", "maserati", "pagani"]
-
-chosen_word = random.choice(word_list)
+    stages = [
+        """
+          +---+
+          |   |
+          O   |
+        /|\\  |
+        / \\  |
+              |
+        =========
+        """,
+        """
+          +---+
+          |   |
+          O   |
+        /|\\  |
+        /    |
+              |
+        =========
+        """,
+        """
+          +---+
+          |   |
+          O   |
+        /|\\  |
+              |
+              |
+        =========
+        """,
+        """
+          +---+
+          |   |
+          O   |
+        /|   |
+              |
+              |
+        =========
+        """,
+        """
+          +---+
+          |   |
+          O   |
+          |   |
+              |
+              |
+        =========
+        """,
+        """
+          +---+
+          |   |
+          O   |
+              |
+              |
+              |
+        =========
+        """,
+        """
+          +---+
+          |   |
+              |
+              |
+              |
+              |
+        =========
+        """
+    ]
 
 
 
-word_lengtn = len(chosen_word)
+    lives = 6
 
-placeholder = ""
+    word_list = ["lamborghini", "porsche", "ferrari", "bugatti", "mcLaren", "astonMartin", "rollsRoyce", "bentley", "maserati", "pagani"]
 
-for possition in range(word_lengtn):
-  placeholder += "_"
+    chosen_word = random.choice(word_list)
 
-print(placeholder)
 
-game_over = False
-correct_letters = []
 
-while not game_over:
+    word_lengtn = len(chosen_word)
 
-  guess = input("Guess The latter: ").lower()
+    placeholder = ""
 
-  if guess in correct_letters:
-    print(f"You've already guessed {guess}. Try again.")
-    lives-=1
-    print(f'{lives} lives left.')
-    if lives == 0:
-      print("You lose.")
-      game_over= True
-      print(f"The word was: {chosen_word}")
+    for possition in range(word_lengtn):
+      placeholder += "_"
 
-  display = ""
+    print(placeholder)
 
-  for latters in chosen_word:
-    if guess in latters:
-      display += latters
+    game_over = False
+    correct_letters = []
 
-      correct_letters.append(guess)
-    elif latters in correct_letters:
-      display += latters
-    else:
-      display += "_"
+    while not game_over:
 
-    
-  print(display)
+      guess = input("Guess The latter: ").lower()
 
-  if guess not in chosen_word:
-    lives-=1
-    print(stages[lives])
-    print(f'{lives} lives left.')
-    if lives == 3:
-      print("⚠️ You’re about to lose! Only 3 lives left.")
-    elif lives==1:
-      print("😨 Last chance! Just 1 life left.")
-    if lives == 0:
-      print("💀 You’re out of lives! You lose.")
-      print(f"The word was: {chosen_word}")
-      game_over= True
-      print("You lose.")
+      if guess in correct_letters:
+        print(f"You've already guessed {guess}. Try again.")
+        lives-=1
+        print(f'{lives} lives left.')
+        if lives == 0:
+          print("You lose.")
+          game_over= True
+          print(f"The word was: {chosen_word}")
 
-  if "_" not in display:
-          game_over = True
-          print("\n🎉 You win!")
-          print("""
-            +---+
-            |   |
-                |
-            😄  |
-                |
-                |
-          =========
-          """)
+      display = ""
 
+      for latters in chosen_word:
+        if guess in latters:
+          display += latters
+
+          correct_letters.append(guess)
+        elif latters in correct_letters:
+          display += latters
+        else:
+          display += "_"
+
+        
+      print(display)
+
+      if guess not in chosen_word:
+        lives-=1
+        print(stages[lives])
+        print(f'{lives} lives left.')
+        if lives == 3:
+          print("⚠️ You’re about to lose! Only 3 lives left.")
+        elif lives==1:
+          print("😨 Last chance! Just 1 life left.")
+        if lives == 0:
+          print("💀 You’re out of lives! You lose.")
+          print(f"The word was: {chosen_word}")
+          game_over= True
+          print("You lose.")
+          
+
+      if "_" not in display:
+              game_over = True
+              print("\n🎉 You win!")
+              print("""
+                +---+
+                |   |
+                    |
+                😄  |
+                    |
+                    |
+              =========
+              """)
+hangMan_game()
+
+print("..............\n")
+
+ask = input("Do you want to play again? (yes/no): ").lower()
+
+if ask == "yes":
+    hangMan_game()
+else:
+    print("Thanks for playing! Goodbye!")
